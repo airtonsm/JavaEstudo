9f8a19624108a3426a36339cb2b385a86693d104
airtonsm <airtonsmorais28@gmail.com>
2020-12-18 20:17:24 -0300

    autenticacao e redirecionamento filter

--- WebContent/autenticar.jsp
+++ WebContent/autenticar.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +8,22 @@
 </head>
 <body>
 	<h1>Autenticar Usuário</h1>
+
+	<form action="ServletAutenticacao" method="post">
+		<table>
+			<tr>
+				<td>Login:</td>
+				<td><input type="text" id="login" name="login"></td>
+			</tr>
+			<tr>
+				<td>Senha:</td>
+				<td><input type="password" id="senha" name="senha"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" id="logar" name="logar" value="Logar"></td>
+			</tr>
+		</table>
+
+	</form>
 </body>
 </html>
---
+++ src/servlet/ServletAutenticacao.java
@@ -0,0 +1,58 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import user.UserLogado;
+
+@WebServlet("/pages/ServletAutenticacao")
+public class ServletAutenticacao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public ServletAutenticacao() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		// pode ser realizado validação no banco de dados
+		if(login.equalsIgnoreCase("admin") && senha.equalsIgnoreCase("123")) {
+			// se login efetuado com sucesso
+			
+			UserLogado userLogado = new UserLogado();
+			userLogado.setLogin(login);
+			userLogado.setSenha(senha);
+			
+			// adiciona usuario logado na sessão
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+			session.setAttribute("usuario", userLogado);
+			
+			//redirecionar para o sistema e autorizar
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/acessoAoSistema.jsp");
+			dispatcher.forward(request, response);
+		}else {
+			// redirecionar para login novamente caso login falhar
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/autenticar.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+	}
+
+}
