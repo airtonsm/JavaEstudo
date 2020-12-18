db3ad7041f439c4eb82c404e5f6b22dffb6c65ca
airtonsm <airtonsmorais28@gmail.com>
2020-12-17 21:33:35 -0300

    add Filter

---
+++ src/filter/FilterAutenticacao.java
@@ -0,0 +1,56 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import user.UserLogado;
+
+@WebFilter(urlPatterns= {"/pages/acessoAoSistema.jsp"})
+public class FilterAutenticacao implements Filter{
+	
+	// faz alguma coisa quando a aplicação é derrubada;
+	@Override
+	public void destroy() {
+		
+	}
+	
+	// intercepta todas as nossas requisições;
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {	
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		//retorna null caso não esteja logado
+		UserLogado userLogado = (UserLogado) session.getAttribute("usuario");
+		
+		if(userLogado == null) {//usuário não logado
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/autenticar.jsp");
+			dispatcher.forward(request, response);
+			return ;// para  o processo redirecionar
+		}
+		
+		//executa as ações de request e response
+		chain.doFilter(request, response);
+		
+		System.out.println("Interceptando");
+	}
+	
+	// executa alguma coisa quando a aplicação for iniciada;
+	@Override
+	public void init(FilterConfig arg0) throws ServletException{
+	}
+	
+}
