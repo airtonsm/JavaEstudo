081e726800732283456c1c9cb58d379b68c3c4b7
airtonsm <airtonsmorais28@gmail.com>
2020-12-26 23:20:52 -0300

    Config connection DB

--- src/connection/ConnectionDataBase.java
+++ src/connection/ConnectionDataBase.java
@@ -12,7 +12,7 @@ public class ConnectionDataBase {
 	
 	private static String banco = "jdbc:postgreslq://localhost:5432/curso-jsp?autoReconnect=true";
 	private static String password = "admin";
-	private static String user = "admin";
+	private static String user = "postgres";
 	private static Connection connection = null;
 	
 	static {
@@ -23,7 +23,7 @@ public class ConnectionDataBase {
 		conectar();
 	}
 	
-	public static conectar() {
+	private static void conectar() {
 		
 		try {
 			if(connection == null)	{
@@ -34,4 +34,13 @@ public class ConnectionDataBase {
 			throw new RuntimeException("Erro ao conectar ao banco de dados" + exception.getMessage());
 		}
 	}
+	/*
+	 * Retorna a conexão do banco de dados
+	 * @return Connection SQL
+	 */
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	
 }
